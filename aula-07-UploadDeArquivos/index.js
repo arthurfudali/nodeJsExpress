f2d8b394a45268fdc687f1d2389dee1cc4665dba
Arthur Fudali <arthurfudali5@gmail.com>
2024-11-06 19:56:39 -0300

    index ejs e index principal
    criação da pagina base e configurações do projeto na index

--- aula-07-UploadDeArquivos/index.js
+++ aula-07-UploadDeArquivos/index.js
@@ -1,5 +1,19 @@
 import express from "express";
+import multer from "multer";
 const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+const upload = multer({dest: "public/uploads/"});
+app.get("/", (req,res)=>{
+    res.render("index");
+});
+
+app.post("/upload", upload.single("file"), (req,res)=>{
+    res.redirect("/");
+});
+
 const port = 8081;
 app.listen(port, (error)=>{
     if(error){
